c07d9a459e4224b921553c3b1e47955f279f4891
fgeorgan <smaile20117311@mail.ru>
2022-08-29 01:13:38 +0300

    devided by functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,9 +11,10 @@ import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    private static String getFact() throws IOException {
 
-        int randomNumber = (int) (Math.random() * 1000000);
+//        int randomNumber = (int) (Math.random() * 1000000);
+        int randomNumber = 100500;
         String url = "http://numbersapi.com/" + randomNumber + "/trivia";
         System.out.println(url);
 
@@ -32,27 +32,10 @@ public class Main {
         }
         in.close();
 
-        String fact = response.toString();
-        System.out.println(fact);
+        return response.toString();
+    }
 
-        Map<Character, Integer> frequency = new HashMap<Character, Integer>();
-        ArrayList<Character> charsCloseAverage = new ArrayList<Character>();
-        float numOfUniqueSymbols = 0, numOfAllSymbols = 0;
-        int averageFrequency = 0;
-
-        fact = fact.replaceAll("[^\\da-zA-Z]", "");
-
-        for (int i = 0; i < fact.length(); i++) {
-            if (frequency.putIfAbsent(fact.charAt(i), 1) != null) {
-                frequency.replace(fact.charAt(i), frequency.get(fact.charAt(i)) + 1);
-            } else {
-                ++numOfUniqueSymbols;
-            }
-            ++numOfAllSymbols;
-        }
-
-        averageFrequency = Math.round(numOfAllSymbols / numOfUniqueSymbols);
-
+    private static void showAnswer(Map<Character, Integer> frequency, int averageFrequency, ArrayList<Character> charsCloseAverage) {
         System.out.println("Частоты:");
         for (Map.Entry<Character, Integer> entry : frequency.entrySet()) {
             Character key = entry.getKey();
@@ -65,13 +48,16 @@ public class Main {
             System.out.print(value);            // использования StringBuilder.
             System.out.println(" раза");
         }
+    }
+
+    private static void additionalTask(int numOfAllSymbols, int numOfUniqueSymbols, ArrayList<Character> charsCloseAverage) {
 
         System.out.print("\nСреднее значение частоты ");
-        System.out.printf("%.0f",numOfAllSymbols);
+        System.out.print(numOfAllSymbols);
         System.out.print('/');
-        System.out.printf("%.0f", numOfUniqueSymbols);
+        System.out.print(numOfUniqueSymbols);
         System.out.print(" = ");
-        System.out.println(numOfAllSymbols / numOfUniqueSymbols);
+        System.out.println((float) numOfAllSymbols / (float) numOfUniqueSymbols);
         System.out.print("Символы, которые соответствуют условию наиболее близкого значения частоты к среднему значанию: ");
 
         for (int i = 0; i < charsCloseAverage.size() - 1; i++) {
@@ -80,9 +66,33 @@ public class Main {
             System.out.print((int) charsCloseAverage.get(i));
             System.out.print("), ");
         }
+
         System.out.print(charsCloseAverage.get(charsCloseAverage.size() - 1));
         System.out.print('(');
         System.out.print((int) charsCloseAverage.get(charsCloseAverage.size() - 1));
         System.out.print(')');
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Map<Character, Integer> frequency = new HashMap<Character, Integer>();
+        ArrayList<Character> charsCloseAverage = new ArrayList<Character>();
+        int numOfUniqueSymbols = 0, numOfAllSymbols = 0;
+
+        String fact = getFact();
+        System.out.println(fact);
+
+        fact = fact.replaceAll("[^\\da-zA-Z]", "");
+        for (int i = 0; i < fact.length(); i++) {
+            if (frequency.putIfAbsent(fact.charAt(i), 1) != null) {
+                frequency.replace(fact.charAt(i), frequency.get(fact.charAt(i)) + 1);
+            } else {
+                ++numOfUniqueSymbols;
+            }
+            ++numOfAllSymbols;
+        }
+
+        showAnswer(frequency, Math.round(numOfAllSymbols / numOfUniqueSymbols), charsCloseAverage);
+        additionalTask(numOfAllSymbols, numOfUniqueSymbols, charsCloseAverage);
     }
 }
